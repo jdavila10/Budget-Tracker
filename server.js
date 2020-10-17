742b7a0d74472aee9f49a7624fd6e502d774cb8f
Jonathan Echevarria <j.eche589@gmail.com>
2020-10-17 15:28:38 -0400

    api added

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const logger = require("morgan");
-const mongoose = require("mongoose");
 const compression = require("compression");
+const mongoose = require("mongoose");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -15,10 +15,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 // routes
 app.use(require("./routes/api.js"));
